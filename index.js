4a085763aebf11cf12772ea91659ecf5cf1f5010
minhajul islam <mm01882390860@gmail.com>
2026-04-14 07:28:34 +0600

    fix mongodb connection for vercel serverless

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+// CORS configuration
 app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://transcendent-cocada-88d049.netlify.app'
   ]
-}));app.use(express.json());
+}));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ud3uec.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -29,57 +31,88 @@ app.get('/', (req, res) => {
   res.send('HobbyHub Server is running!');
 });
 
+// --- Updated Routes Start ---
+
 app.get('/groups', async (req, res) => {
-  const email = req.query.email;
-  let query = {};
-  if (email) {
-    query = { userEmail: email };
+  try {
+    await client.connect();
+    const email = req.query.email;
+    let query = {};
+    if (email) {
+      query = { userEmail: email };
+    }
+    const result = await groupsCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
   }
-  const result = await groupsCollection.find(query).toArray();
-  res.send(result);
 });
 
 app.get('/groups/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await groupsCollection.findOne(query);
-  res.send(result);
+  try {
+    await client.connect();
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await groupsCollection.findOne(query);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.post('/groups', async (req, res) => {
-  const group = req.body;
-  const result = await groupsCollection.insertOne(group);
-  res.send(result);
+  try {
+    await client.connect();
+    const group = req.body;
+    const result = await groupsCollection.insertOne(group);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.put('/groups/:id', async (req, res) => {
-  const id = req.params.id;
-  const updatedGroup = req.body;
-  const filter = { _id: new ObjectId(id) };
-  const updateDoc = {
-    $set: {
-      groupName: updatedGroup.groupName,
-      category: updatedGroup.category,
-      description: updatedGroup.description,
-      meetingLocation: updatedGroup.meetingLocation,
-      maxMembers: updatedGroup.maxMembers,
-      startDate: updatedGroup.startDate,
-      imageURL: updatedGroup.imageURL,
-    }
-  };
-  const result = await groupsCollection.updateOne(filter, updateDoc);
-  res.send(result);
+  try {
+    await client.connect();
+    const id = req.params.id;
+    const updatedGroup = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        groupName: updatedGroup.groupName,
+        category: updatedGroup.category,
+        description: updatedGroup.description,
+        meetingLocation: updatedGroup.meetingLocation,
+        maxMembers: updatedGroup.maxMembers,
+        startDate: updatedGroup.startDate,
+        imageURL: updatedGroup.imageURL,
+      }
+    };
+    const result = await groupsCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.delete('/groups/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await groupsCollection.deleteOne(query);
-  res.send(result);
+  try {
+    await client.connect();
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await groupsCollection.deleteOne(query);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-client.connect().then(() => {
-  
-}).catch(console.dir);
+// --- Updated Routes End ---
 
+// Vercel বা অন্য সার্ভার হোস্টের জন্য এই লাইনটি গুরুত্বপূর্ণ
 module.exports = app;
+
+// লোকাল পোর্টে চালানোর জন্য (Optional)
+if (require.main === module) {
+  
+}
